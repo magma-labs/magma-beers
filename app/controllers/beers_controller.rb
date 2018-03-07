0ea7e2c79079332d93459f905482c3df38528970
Juan Carlos López <jcarloslopezl@hotmail.com>
2018-03-07 14:18:00 -0600

    Create views and functionality for beers controller

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,14 +1,37 @@
 class BeersController < ApplicationController
-
   before_action :authenticate_user!
+  before_action :set_beer, only: [:show, :edit, :update, :destroy]
 
   def index
-
-    @beers = $brewery_db.search.beers(q: 'Corona')
-    # .beers.find('Tecate')
-    # puts @beers
-
+    @beers = Beer.all
   end
+
+  def show
+  end
+
+  def new
+    @beer = Beer.new
+  end
+
+  def create
+    @beer = Beer.new(beer_params)
+
+    if @beer.save
+      redirect_to @beer, notice: 'The beer was created successfully.'
+    else
+      render :new
+    end
+  end
+
+  private
+    def set_beer
+      @beer = Beer.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def beer_params
+      params.require(:beer).permit(:name, :description, :photo, :status, :abv, :ibu)
+    end
 
 end
 
