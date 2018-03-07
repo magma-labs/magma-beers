65083c78fd8e98405ff2afad89858bbd79629f59
Juan Carlos López <jcarloslopezl@hotmail.com>
2018-03-07 14:06:11 -0600

    Add validations in beers model

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,7 +1,10 @@
 class Beer < ApplicationRecord
-  #   validates_attachment_content_type :photo, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
-  has_attached_file :photo, styles: { thumbnail: "300x300>", thumb: "100x100>" }, default_url: "missing.png"
+  has_attached_file :photo, styles: { thumbnail: "300x300>", thumb: "100x100>" }, default_url: "noimage.jpg"
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
 
   resourcify
+
+  validates_uniqueness_of :name
+  validates_presence_of :name
+  validates_presence_of :description
 end
