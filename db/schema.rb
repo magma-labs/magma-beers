d4e7b2b2f13aa3db8c79eddfaef3f9fca51a011c
Edgar Diaz <edgar.diaz@magmalabs.com>
2018-03-08 14:03:41 -0600

    [ADD] Profile view to main layout
    - Integrate paperclip with image for the user.
    - Integrate paperclip with the avatar on the main layout.
    - Add form on the option settings from the main layout.
    - Add image to user form on administrate layout.

--- db/schema.rb
+++ db/schema.rb
@@ -1,85 +1,85 @@
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
-#
-# It's strongly recommended that you check this file into your version control system.
+  # This file is auto-generated from the current state of the database. Instead
+  # of editing this file, please use the migrations feature of Active Record to
+  # incrementally modify your database, and then regenerate this schema definition.
+  #
+  # Note that this schema.rb definition is the authoritative source for your
+  # database schema. If you need to create the application database on another
+  # system, you should be using db:schema:load, not running all the migrations
+  # from scratch. The latter is a flawed and unsustainable approach (the more migrations
+  # you'll amass, the slower it'll run and the greater likelihood for issues).
+  #
+  # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180309175517) do
+  ActiveRecord::Schema.define(version: 20180309175517) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+    # These are extensions that must be enabled in order to support this database
+    enable_extension "plpgsql"
 
-  create_table "beers", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "photo_file_name"
-    t.string "photo_content_type"
-    t.integer "photo_file_size"
-    t.datetime "photo_updated_at"
-    t.float "abv"
-    t.integer "ibu"
-    t.boolean "status"
-    t.integer "year"
-  end
+    create_table "beers", force: :cascade do |t|
+      t.string "name"
+      t.text "description"
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.string "photo_file_name"
+      t.string "photo_content_type"
+      t.integer "photo_file_size"
+      t.datetime "photo_updated_at"
+      t.float "abv"
+      t.integer "ibu"
+      t.boolean "status"
+      t.integer "year"
+    end
 
-  create_table "identities", force: :cascade do |t|
-    t.bigint "user_id"
-    t.string "provider"
-    t.string "accesstoken"
-    t.string "refreshtoken"
-    t.string "uid"
-    t.string "name"
-    t.string "email"
-    t.string "nickname"
-    t.string "image"
-    t.string "phone"
-    t.string "urls"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_identities_on_user_id"
-  end
+    create_table "identities", force: :cascade do |t|
+      t.bigint "user_id"
+      t.string "provider"
+      t.string "accesstoken"
+      t.string "refreshtoken"
+      t.string "uid"
+      t.string "name"
+      t.string "email"
+      t.string "nickname"
+      t.string "image"
+      t.string "phone"
+      t.string "urls"
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.index ["user_id"], name: "index_identities_on_user_id"
+    end
 
-  create_table "roles", force: :cascade do |t|
-    t.string "name"
-    t.string "resource_type"
-    t.bigint "resource_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
-    t.index ["resource_type", "resource_id"], name: "index_roles_on_resource_type_and_resource_id"
-  end
+    create_table "roles", force: :cascade do |t|
+      t.string "name"
+      t.string "resource_type"
+      t.bigint "resource_id"
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
+      t.index ["resource_type", "resource_id"], name: "index_roles_on_resource_type_and_resource_id"
+    end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet "current_sign_in_ip"
-    t.inet "last_sign_in_ip"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "provider"
-    t.string "uid"
-    t.string "name"
-    t.string "image"
-    t.string "photo_file_name"
-    t.string "photo_content_type"
-    t.integer "photo_file_size"
-    t.datetime "photo_updated_at"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    create_table "users", force: :cascade do |t|
+      t.string "email", default: "", null: false
+      t.string "encrypted_password", default: "", null: false
+      t.string "reset_password_token"
+      t.datetime "reset_password_sent_at"
+      t.datetime "remember_created_at"
+      t.integer "sign_in_count", default: 0, null: false
+      t.datetime "current_sign_in_at"
+      t.datetime "last_sign_in_at"
+      t.inet "current_sign_in_ip"
+      t.inet "last_sign_in_ip"
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.string "provider"
+      t.string "uid"
+      t.string "name"
+      t.string "image"
+      t.string "photo_file_name"
+      t.string "photo_content_type"
+      t.integer "photo_file_size"
+      t.datetime "photo_updated_at"
+      t.index ["email"], name: "index_users_on_email", unique: true
+      t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "users_roles", id: false, force: :cascade do |t|
