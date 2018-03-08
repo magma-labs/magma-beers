64643458f7f26ea32c7af733d9164c1e5b8a6561
Edgar Diaz <edgar.diaz@magmalabs.com>
2018-03-08 14:03:41 -0600

    [ADD] Profile view to main layout
    - Integrate paperclip with image for the user.
    - Integrate paperclip with the avatar on the main layout.
    - Add form on the option settings from the main layout.
    - Add image to user form on administrate layout.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180306212114) do
+ActiveRecord::Schema.define(version: 20180307231949) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
