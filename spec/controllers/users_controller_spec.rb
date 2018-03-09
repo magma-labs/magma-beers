4f11ed216b91cddc92ddc880ece1d17491d85b0a
Edgar Diaz <edgar.diaz@magmalabs.com>
2018-03-09 12:11:44 -0600

    [REF] change field for paperclip
    Use photo as name filed for paperclip

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,10 +1,21 @@
 require 'rails_helper'
 
 describe UsersController, type: :controller do
-  let(:attr) do
-    {
-      :name => "Edgar1", :password => "",
-      :password_confirmation => "", :current_password => ""
-    }
+  let(:user) { create(:user) }
+  let(:admin) { create(:admin) }
+  # let(:attr_without_pass) do
+  #   {
+  #     :name => FFaker::Name.name, :password => "",
+  #     :password_confirmation => "", :current_password => ""
+  #   }
+  # end
+
+  it "On succes update user profile redirect to settings page" do
+    sign_in user
+    put :update, :params => { :id => user.id, :user => { name: FFaker::Name.name } }
+    user.reload
+
+    expect(response.status).to eq(302)
+    expect(response).to redirect_to edit_user_url
   end
 end
---
+++ spec/features/admin/settings_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+feature 'Admin settings', js: true do
+  scenario 'admin can\'t cancel his account' do
+  end
+end
