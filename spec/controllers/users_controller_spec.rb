e1c19be989f8e905de79e20f403d66d35a9a4c26
Edgar Diaz <edgar.diaz@magmalabs.com>
2018-03-12 11:52:22 -0600

    [ADD] Update user profile with password

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,18 +2,17 @@ require 'rails_helper'
 
 describe UsersController, type: :controller do
   let(:user) { create(:user) }
-  let(:admin) { create(:admin) }
-  # let(:attr_without_pass) do
-  #   {
-  #     :name => FFaker::Name.name, :password => "",
-  #     :password_confirmation => "", :current_password => ""
-  #   }
-  # end
+  let(:new_pass) { FFaker::InternetSE.password }
+  let(:attr) do
+    {
+      :name => FFaker::Name.name, :password => "",
+      :password_confirmation => "", :current_password => ""
+    }
+  end
 
   it "On succes update user profile redirect to settings page" do
     sign_in user
-    put :update, :params => { :id => user.id, :user => { name: FFaker::Name.name } }
-    user.reload
+    put :update, :params => { :id => user.id, :user => attr }
 
     expect(response.status).to eq(302)
     expect(response).to redirect_to edit_user_url
