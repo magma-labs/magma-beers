aa0a6a6c0bd338646b610b98da48ff0a5bcf0f0a
Daniel Haro <dannharo@hotmail.com>
2018-03-13 13:02:40 -0600

    [ADD] Log Beers add controller of log beers add model of log beer add beer log view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,13 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_data
 
   def index
-
+    @beerlog = BeerLog.new
   end
-  
-end
 
+  private
+  def set_data
+    @beerlogs = BeerLog.where("user_id = ? and created_at >= ?", current_user.id,Time.zone.now.beginning_of_day).order(:id)
+  end
+end
