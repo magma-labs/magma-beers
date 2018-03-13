f51ad9fa20666546bea5af1a0e16d80de62553d8
Edgar Diaz <edgar.diaz@magmalabs.com>
2018-03-13 10:20:48 -0600

    [FIX] Adjust for code review

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe UsersController, type: :controller do
   let(:user) { create(:user) }
   let(:new_pass) { FFaker::InternetSE.password }
-  let(:attr) do
+  let(:user_form_attr) do
     {
       :name => FFaker::Name.name, :password => "",
       :password_confirmation => "", :current_password => ""
@@ -12,7 +12,7 @@ describe UsersController, type: :controller do
 
   it "On succes update user profile redirect to settings page" do
     sign_in user
-    put :update, :params => { :id => user.id, :user => attr }
+    put :update, :params => { :id => user.id, :user => user_form_attr }
 
     expect(response.status).to eq(302)
     expect(response).to redirect_to edit_user_url
