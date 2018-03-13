6fde84fdc03ed0bcde5f937880143ae3ecef1fa5
Edgar Diaz <edgar.diaz@magmalabs.com>
2018-03-13 10:20:48 -0600

    [FIX] Adjust for code review

--- app/fields/paperclip_field.rb
+++ app/fields/paperclip_field.rb
@@ -1,24 +1,12 @@
 require "administrate/field/base"
 
 class PaperclipField < Administrate::Field::Base
-  # class Engine < ::Rails::Engine
-  # end
-
   def url
     data.url
   end
 
-  # def style(size = thumbnail)
-  #   binding.pry
-  #   # data.try(:url, size) || ''
-  #   data.url(size)
-  # end
-
-  # delegate :url, to: :data
-
   def thumbnail
     data.url(:thumbnail)
-    # style(thumbnail_style)
   end
 
   def thumb
@@ -88,27 +76,4 @@ class PaperclipField < Administrate::Field::Base
   def to_s
     data
   end
-
-  # Just display the URL as a link, rather than trying to make it an image
-  # def url_only?
-  #   binding.pry
-  #   options.fetch(:url_only, false)
-  # end
-  #
-  # alias url style
-  #
-  # private
-  #
-  # DEFAULT_THUMBNAIL_STYLE = :thumbnail
-  # DEFAULT_BIG_STYLE = :original
-  #
-  # def thumbnail_style
-  #   binding.pry
-  #   options.fetch(:thumbnail_style, DEFAULT_THUMBNAIL_STYLE)
-  # end
-  #
-  # def big_style
-  #   binding.pry
-  #   options.fetch(:big_style, DEFAULT_BIG_STYLE)
-  # end
 end
