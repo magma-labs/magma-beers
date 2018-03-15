16627c0e0cab8671c55d388ded69c956ea0dbcbd
Edgar Diaz <edgar.diaz@magmalabs.com>
2018-03-15 13:59:07 -0600

    [REF] move spec code to a before block

--- spec/features/admin/settings_spec.rb
+++ spec/features/admin/settings_spec.rb
@@ -1,18 +1,17 @@
 require 'rails_helper'
 
 feature 'Admin settings', js: true do
-  scenario 'admin can\'t see cancel account section' do
+  before do
     sign_in(:admin)
     visit "/users/#{User.first.id}/edit"
     expect(page).to have_content("Edit Profile")
+  end
+
+  scenario 'admin can\'t see cancel account section' do
     expect(page).to have_no_content("Cancel Account User")
   end
 
   scenario 'update admin profile' do
-    sign_in(:admin)
-    visit "/users/#{User.first.id}/edit"
-    expect(page).to have_content("Edit Profile")
-
     fill_in 'user_name', with: FFaker::Name.name
     click_button 'Update'
     expect(page).to have_content("Edit Profile")
