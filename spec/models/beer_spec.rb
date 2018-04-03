2233d8f21f5da0fd1f5b110f9686b034c4ba79ab
Edgar Diaz <edgar.diaz@magmalabs.io>
2018-04-03 16:54:26 -0500

    delete unused specs

--- spec/models/beer_spec.rb
+++ spec/models/beer_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe Beer, type: :model do
-  let(:beer) { build(:beer, photo_remote_url: 'http://www.estucerveza.com/productos/imagenes/img_929_bf5cd22e3afde9fe3827e0c0dfdebe97_1.jpg') }
+  let(:beer) { build(:beer) }
   subject { beer }
 
   it { should be_valid }
